0a3eddb9ed2700339589a37b73a90c90b09af4d2
이혜진 <floral_berry@naver.com>
2024-03-14 14:06:29 +0900

    [feat] 24.03.14 Account entity
    Change-Id: I286cf86337709fd977cd8b132216384cc64d63a6

--- backend/src/main/java/com/yoyak/yoyak/account/domain/Account.java
+++ backend/src/main/java/com/yoyak/yoyak/account/domain/Account.java
@@ -1,4 +1,49 @@
 package com.yoyak.yoyak.account.domain;
 
+import com.yoyak.yoyak.user.domain.User;
+import com.yoyak.yoyak.user.domain.UserGender;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false, length = 32)
+    private String name;
+
+    @Column(nullable = false, length = 32)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserGender gender;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(nullable = true)
+    private String disease;
+
+    @Column(nullable = false)
+    private int profileImg;
+
+    @Column(nullable = false)
+    private AccountRole role;
+
+    @ManyToOne
+    @JoinColumn(name = "user_seq", nullable = false)
+    private User user;
+
 }
---
+++ backend/src/main/java/com/yoyak/yoyak/account/domain/AccountRole.java
@@ -0,0 +1,5 @@
+package com.yoyak.yoyak.account.domain;
+
+public enum AccountRole {
+    ADMIN, CARE
+}
