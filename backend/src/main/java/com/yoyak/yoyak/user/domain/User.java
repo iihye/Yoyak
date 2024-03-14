1938977e50c48dca94f219978b3c6ff46ebdd282
이혜진 <floral_berry@naver.com>
2024-03-14 14:05:02 +0900

    [feat] 24.03.14 User entity
    Change-Id: I0b7330aa61198b3e1de5998d7227813909cba6a1

--- backend/src/main/java/com/yoyak/yoyak/user/domain/User.java
+++ backend/src/main/java/com/yoyak/yoyak/user/domain/User.java
@@ -1,4 +1,43 @@
 package com.yoyak.yoyak.user.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 32)
+    private String name;
+
+    @Column(nullable = false, length = 32)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserGender gender;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserPlatform platform;
 }
