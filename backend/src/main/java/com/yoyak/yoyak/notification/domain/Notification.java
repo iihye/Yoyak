4f56fd3f75a822c29b0561368578461f475eb30e
이혜진 <floral_berry@naver.com>
2024-03-14 14:06:49 +0900

    [feat] 24.03.14 Notification entity
    Change-Id: I46df49231c77fedfb4adcd3b8ab13276ca809e4b

--- backend/src/main/java/com/yoyak/yoyak/notification/domain/Notification.java
+++ backend/src/main/java/com/yoyak/yoyak/notification/domain/Notification.java
@@ -1,4 +1,45 @@
 package com.yoyak.yoyak.notification.domain;
 
+import com.yoyak.yoyak.account.domain.Account;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "notification")
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private List<DayOfWeek> period;
+
+    @Column(nullable = false)
+    private LocalDate createDate;
+
+    @Column(nullable = true)
+    private LocalDate modifyDate;
+
+    @ManyToOne
+    @JoinColumn(name = "account_seq", nullable = false)
+    private Account account;
+
 }
--- backend/src/main/java/com/yoyak/yoyak/notificationTime/domain/NotificationTime.java
+++ backend/src/main/java/com/yoyak/yoyak/notificationTime/domain/NotificationTime.java
@@ -1,4 +1,32 @@
 package com.yoyak.yoyak.notificationTime.domain;
 
+import com.yoyak.yoyak.notification.domain.Notification;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "notification_time")
 public class NotificationTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalDate takenDate;
+
+    @ManyToOne
+    @JoinColumn(name = "notification_seq", nullable = false)
+    private Notification notification;
+
 }
