6b8dbaeaae185aac90cd450fd170994c7a2dcadb
신성현 <froggy0m0a@gmail.com>
2024-03-18 02:21:15 +0900

    [feat] 2024.03.18 필터링검색 구현
    Change-Id: I4242085da7b4e092e4940f054c585bc14297521f

--- backend/src/main/java/com/yoyak/yoyak/medicine/controller/MedicineController.java
+++ backend/src/main/java/com/yoyak/yoyak/medicine/controller/MedicineController.java
@@ -1,4 +1,39 @@
 package com.yoyak.yoyak.medicine.controller;
 
+import com.yoyak.yoyak.medicine.dto.MedicineDto;
+import com.yoyak.yoyak.medicine.dto.MedicineSearchParametersDto;
+import com.yoyak.yoyak.medicine.service.MedicineService;
+import com.yoyak.yoyak.util.dto.BasicResponseDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/medicine")
+@RequiredArgsConstructor
+@Slf4j
 public class MedicineController {
+
+    private final MedicineService medicineService;
+
+    @GetMapping("/filter")
+    public BasicResponseDto orderList(@ModelAttribute MedicineSearchParametersDto parameters) {
+
+        log.info("[{}.{}] parmeters = {}", this.getClass().getName(),
+            Thread.currentThread().getStackTrace()[1].getMethodName(), parameters);
+
+        List<MedicineDto> medicineList = medicineService.findOrderParameters(parameters);
+
+        log.info("[{}.{}] medicineList = {}", this.getClass().getName(),
+            Thread.currentThread().getStackTrace()[1].getMethodName(), medicineList);
+
+        return BasicResponseDto.builder()
+            .count(medicineList.size())
+            .result(medicineList)
+            .build();
+    }
 }
--- backend/src/main/java/com/yoyak/yoyak/medicine/domain/MedicineRepository.java
+++ backend/src/main/java/com/yoyak/yoyak/medicine/domain/MedicineRepository.java
@@ -1,4 +1,27 @@
 package com.yoyak.yoyak.medicine.domain;
 
-public interface MedicineRepository {
+import com.yoyak.yoyak.medicine.dto.MedicineDto;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Integer> {
+
+    Optional<Medicine> findBySeq(Long Seq);
+
+    @Query(
+        "SELECT new com.yoyak.yoyak.medicine.dto.MedicineDto(m.seq, m.imgPath, m.itemName, m.entpName)"
+            + " FROM MedicineDetail md JOIN md.medicine m "
+            + "WHERE md.drugShape = :drugShape "
+            + "AND (:colorClass IS NULL OR md.colorClass1 = :colorClass OR md.colorClass2 = :colorClass) "
+            + "AND (:formCodeName IS NULL OR md.formCodeName LIKE CONCAT('%', :formCodeName, '%')) "
+            + "AND (:line IS NULL OR md.lineFront = :line OR md.lineBack = :line)")
+    List<MedicineDto> findByParameters(
+        @Param("drugShape") String drugShape,
+        @Param("colorClass") String colorClass,
+        @Param("formCodeName") String formCodeName,
+        @Param("line") String line
+    );
 }
