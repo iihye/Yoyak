acbd83b416048d13f2b3e67de18face234ed45f5
김성현 <oilater@naver.com>
2024-03-18 17:58:41 +0900

    [feat] 메인페이지지 디자인 및 컴포넌트 배치
    Change-Id: I40c0fb6eb27f35b6f3a634faa5f1168836422b07

--- frontend/yoyak/lib/screen/Home/home_screen.dart
+++ frontend/yoyak/lib/screen/Home/home_screen.dart
@@ -9,17 +9,78 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: MainAppBar(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text("Asdf", style: TextStyle(
-              color: Colors.black
-            ),)
+            Stack(children: [
+              Container(
+                width: screenWidth,
+                height: 180,
+                color: Colors.blueAccent,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "성현님 건강하세요",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Pretendard',
+                        ),
+                      ),
+                      RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                          text: '오늘 드실 약은 ',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Pretendard',
+                          ),
+                        ),
+                        TextSpan(
+                          text: '3개',
+                          style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.yellow,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Pretendard',
+                          ),
+                        ),
+                        TextSpan(
+                          text: '에요',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Pretendard',
+                          ),
+                        ),
+                      ])),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                  top: 130, // 컨테이너의 위에서부터의 거리
+                  child: Container(
+                    width: screenWidth,
+                    height: 500,
+                    color: Colors.white,
+                    child: Text("asdf"),
+                  ))
+            ])
           ],
         ),
       ),
