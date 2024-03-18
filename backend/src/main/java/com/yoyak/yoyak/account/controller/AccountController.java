eb55fcf73a8a4547a7b0ccf5685d24a6cdda7525
이혜진 <floral_berry@naver.com>
2024-03-18 17:14:33 +0900

    [feat] 24.03.18 spring api test
    Change-Id: I91471166ef03dd70b88ac01a6612d9c95324d736

--- backend/src/main/java/com/yoyak/yoyak/account/controller/AccountController.java
+++ backend/src/main/java/com/yoyak/yoyak/account/controller/AccountController.java
@@ -1,4 +1,21 @@
 package com.yoyak.yoyak.account.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
 public class AccountController {
+
+    @GetMapping("/test")
+    public ResponseEntity<?> boardTest() {
+        log.info("test");
+        return ResponseEntity.ok("boardTest");
+    }
 }
