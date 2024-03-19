46d2aa51cd737433f8aee0463180ba7c50968002
김성현 <oilater@naver.com>
2024-03-19 10:47:00 +0900

    [feat] 컴포넌트 생성
    Change-Id: Ic061a50b564d30ca25d5e581101a0ecf0458acfe

---
+++ frontend/yoyak/lib/components/RoundedRectangle.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:yoyak/screen/Alarm/alarm_screen.dart';
+import 'package:yoyak/screen/Mypage/mypage_screen.dart';
+
+class RoundedRectangle extends StatelessWidget {
+  final double width, height;
+  final destination;
+  // final Color color;
+  final Widget child;
+  const RoundedRectangle({super.key, required this.width, required this.height, required this.child, required this.destination});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => destination));
+    },
+
+      child: Container (
+        margin: EdgeInsets.only(bottom: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          // border: Border.all(
+          //   width: 0.1,
+          //   color: Colors.white,
+          // ),
+          // boxShadow: [
+          //   BoxShadow(
+          //       color: Color(0x00b5b5b5).withOpacity(0.1),
+          //       offset: Offset(0.1, 0.1),
+          //       blurRadius: 3// 그림자 위치 조정
+          //   ),
+          // ]
+        ),
+        width: width,
+        height: height,
+        child: child,
+      )
+    );
+  }
+}
