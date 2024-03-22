88e2b8f6294aebd03b8afd0bdef7fd0397609f02
김희수 <wlskaka4@gmail.com>
2024-03-22 17:50:29 +0900

    [fix] 24.03.22 summaryTest 오류 í제거
    Change-Id: Icf11e8ce58d60d27fc7731d5d2fc643bf9100147

--- backend/src/test/java/com/yoyak/yoyak/python/service/PythonServiceTest.java
+++ backend/src/test/java/com/yoyak/yoyak/python/service/PythonServiceTest.java
@@ -1,28 +1,24 @@
 package com.yoyak.yoyak.python.service;
 
-import com.yoyak.yoyak.medicineDetail.domain.MedicineDetail;
-import com.yoyak.yoyak.medicineDetail.domain.MedicineDetailRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PythonServiceTest {
 
-    @Autowired
-    private PythonService pythonService;
-
-    @Autowired
-    private MedicineDetailRepository medicineDetailRepository;
-
-    @Test
+    //    @Autowired
+//    private PythonService pythonService;
+//
+//    @Autowired
+//    private MedicineDetailRepository medicineDetailRepository;
+//
+//    @Test
     public void summaryTest() {
-        Long itemSeq = 202106092L;
-
-        MedicineDetail medicineDetail = medicineDetailRepository.findBySeq(itemSeq)
-            .orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다. itemSeq = " + itemSeq));
-
-        System.out.println(pythonService.getSummary(medicineDetail));
+//        Long itemSeq = 202106092L;
+//
+//        MedicineDetail medicineDetail = medicineDetailRepository.findBySeq(itemSeq)
+//            .orElseThrow(() -> new IllegalArgumentException("해당 데이터가 없습니다. itemSeq = " + itemSeq));
+//
+//        System.out.println(pythonService.getSummary(medicineDetail));
     }
 
 }
