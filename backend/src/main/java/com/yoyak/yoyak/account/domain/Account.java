21a12e9cbefe4b54b2ab1618785af038731461ba
이혜진 <floral_berry@naver.com>
2024-03-25 12:46:55 +0900

    [feat] 24.03.25 OneToMany entity 추가
    Change-Id: I20b10a0f05e8d8de1b856f40798b1b55758587f2

--- backend/src/main/java/com/yoyak/yoyak/account/domain/Account.java
+++ backend/src/main/java/com/yoyak/yoyak/account/domain/Account.java
@@ -1,19 +1,25 @@
 package com.yoyak.yoyak.account.domain;
 
 import com.yoyak.yoyak.account.dto.AccountModifyDto;
+import com.yoyak.yoyak.medicineEnvelop.domain.MedicineEnvelop;
+import com.yoyak.yoyak.notification.domain.Notification;
 import com.yoyak.yoyak.user.domain.User;
 import com.yoyak.yoyak.user.domain.UserGender;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -58,6 +64,12 @@ public class Account {
     @ManyToOne
     @JoinColumn(name = "user_seq", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<MedicineEnvelop> medicineEnvelops;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Notification> notifications;
 
     public void modifyAccount(AccountModifyDto accountModifyDto) {
         this.nickname = accountModifyDto.getNickname();
