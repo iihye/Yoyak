683b9d1a1ac736759bad00c07b0335ddfac0f6c9
신성현 <froggy0m0a@gmail.com>
2024-03-27 00:37:35 +0900

    [feat] 24.03.27 약 상세보기 - 주의사항 키워드 추가
    Change-Id: Ia01e3efcd83003ecdb6b8183c6c2c171aa9860d6

--- backend/src/main/java/com/yoyak/yoyak/medicineDetail/service/MedicineDetailService.java
+++ backend/src/main/java/com/yoyak/yoyak/medicineDetail/service/MedicineDetailService.java
@@ -7,6 +7,10 @@ import com.yoyak.yoyak.medicineDetail.domain.MedicineDetailRepository;
 import com.yoyak.yoyak.medicineDetail.dto.MedicineDetailDto;
 import com.yoyak.yoyak.python.service.PythonService;
 import com.yoyak.yoyak.util.exception.CustomException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -32,7 +36,8 @@ public class MedicineDetailService {
 
         log.info("medicineDetail ={}", medicineDetail.getMedicine().getImgPath());
 
-        String summary = pythonService.getSummary(medicineDetail);
+//        String summary = pythonService.getSummary(medicineDetail);
+        String keyword = mapKeywords(medicineDetail.getAtpnWarn() + medicineDetail.getAtpn());
 
         MedicineDetailDto build = MedicineDetailDto.builder()
             .medicineSeq(medicineDetail.getSeq())
@@ -44,11 +49,29 @@ public class MedicineDetailService {
             .atpnWarn(medicineDetail.getAtpnWarn())
             .atpn(medicineDetail.getAtpn())
             .sideEffect(medicineDetail.getSideEffect())
-            .summary(summary)
+//            .summary(summary)
+            .keyword(keyword)
             .build();
 
         log.info("result={}", build);
 
         return build;
+    }
+
+    private String mapKeywords(String text) {
+        Map<String, String[]> keywordMap = new HashMap<>();
+        keywordMap.put("임산부", new String[]{"임부", "임신하고", "임신", "임산부"});
+        keywordMap.put("아이", new String[]{"소아", "유아", "어린이", "미숙아", "영아", "젖먹이", "갓난아기", "신생아"});
+        keywordMap.put("고령자", new String[]{"고령자", "노인"});
+        keywordMap.put("졸음", new String[]{"졸음"});
+        keywordMap.put("어지러움", new String[]{"어지러움", "현기증"});
+
+        // 매칭된 키워드 찾기를 스트림으로 처리
+        String matchedKeywords = keywordMap.entrySet().stream()
+            .filter(entry -> Arrays.stream(entry.getValue()).anyMatch(text::contains))
+            .map(Map.Entry::getKey)
+            .collect(Collectors.joining(", "));
+
+        return matchedKeywords.isEmpty() ? null : matchedKeywords + " 주의";
     }
 }
