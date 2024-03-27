da6a90488bb23681a097cdb6a1693987073cef57
이혜진 <floral_berry@naver.com>
2024-03-27 17:35:48 +0900

    [fix] 24.03.27 Nginx 무중단 배포 서버 rollback
    Change-Id: I93cb388f903ab3919fcf81300e04e1004e8f2d73

--- backend/src/main/java/com/yoyak/yoyak/YoyakApplication.java
+++ backend/src/main/java/com/yoyak/yoyak/YoyakApplication.java
@@ -1,18 +1,22 @@
 package com.yoyak.yoyak;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class YoyakApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-        + "classpath:application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(YoyakApplication.class)
-            .properties(APPLICATION_LOCATIONS)
-            .run(args);
+        SpringApplication.run(YoyakApplication.class, args);
     }
+
+//    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//        + "classpath:application.yml";
+//
+//    public static void main(String[] args) {
+//        new SpringApplicationBuilder(YoyakApplication.class)
+//            .properties(APPLICATION_LOCATIONS)
+//            .run(args);
+//    }
 
 }
--- backend/src/main/java/com/yoyak/yoyak/util/server/WebRestController.java
+++ backend/src/main/java/com/yoyak/yoyak/util/server/WebRestController.java
@@ -1,23 +1,23 @@
-package com.yoyak.yoyak.util.server;
-
-import java.util.Arrays;
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/test")
-public class WebRestController {
-
-    private final Environment env;
-
-    @GetMapping("/profile")
-    public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-            .findFirst()
-            .orElse("");
-    }
-}
+//package com.yoyak.yoyak.util.server;
+//
+//import java.util.Arrays;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.core.env.Environment;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/test")
+//public class WebRestController {
+//
+//    private final Environment env;
+//
+//    @GetMapping("/profile")
+//    public String getProfile() {
+//        return Arrays.stream(env.getActiveProfiles())
+//            .findFirst()
+//            .orElse("");
+//    }
+//}
