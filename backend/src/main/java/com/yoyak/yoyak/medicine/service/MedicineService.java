d4ef7aa5ffbe52fe3c72ec62731b06ac7ca0e994
신성현 <froggy0m0a@gmail.com>
2024-03-27 16:20:27 +0900

    [refactor] 24.03.27 이미지경로에 s3주소 prefix 설정
    Change-Id: Ifd1558978f6d7db582948effdd5bfd5173d4ddbb

--- backend/src/main/java/com/yoyak/yoyak/medicine/service/MedicineService.java
+++ backend/src/main/java/com/yoyak/yoyak/medicine/service/MedicineService.java
@@ -11,6 +11,7 @@ import com.yoyak.yoyak.util.exception.CustomExceptionStatus;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +20,9 @@ import org.springframework.stereotype.Service;
 public class MedicineService {
 
     private final MedicineRepository medicineRepository;
+
+    @Value("${cloud.aws.s3.prefix}")
+    private String s3Prefix;
 
     /**
      * 주어진 고유 번호(seq)를 사용하여 약을 조회하고, 조회된 약의 정보를 반환
@@ -29,7 +33,8 @@ public class MedicineService {
     public MedicineDto findMedicine(Long seq) {
         Medicine medicine = medicineRepository.findBySeq(seq)
             .orElseThrow(() -> new CustomException(MEDICINE_NOT_EXIST));
-        return new MedicineDto(medicine.getSeq(), medicine.getImgPath(), medicine.getItemName(),
+        return new MedicineDto(medicine.getSeq(), s3Prefix + medicine.getImgPath(),
+            medicine.getItemName(),
             medicine.getEntpName());
     }
 
@@ -53,11 +58,10 @@ public class MedicineService {
             throw new CustomException(CustomExceptionStatus.MEDICINE_NOT_FOUND);
         }
 
-        return medicineRepository.findByParameters(
-            parameters.getSearchName(),
-            parameters.getDrugShape(),
-            parameters.getColorClass(),
-            parameters.getFormCodeName(),
-            parameters.getLine());
+        // s3주소 넣어주기
+        result.stream()
+            .forEach(dto -> dto.setImgPath(s3Prefix + dto.getImgPath()));
+
+        return result;
     }
 }
