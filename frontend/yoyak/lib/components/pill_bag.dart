0e7776b571cfdbdb529d38c8b8744bf0a5090a24
Jiwon <yjw5602@naver.com>
2024-03-28 10:44:51 +0900

    [feat] 24.03.28  약 봉투 모달 생성
    Change-Id: I299800ca1c72005d202c1df4879ef8ea94a905f4

--- frontend/yoyak/lib/components/pill_bag.dart
+++ frontend/yoyak/lib/components/pill_bag.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yoyak/styles/colors/palette.dart';
 
 class PillBag extends StatefulWidget {
   final String envelopName;
   final int medicineSeq; // 필요한가? 해당 약이 들어있는지 check할 때 사용
   final int accountSeq;
   final String nickname;
+  final String color;
   final bool isSavedMedicine; // 해당 약이 들어있는지 true/false로 check
   final VoidCallback onClick; // 선택 시 실행할 콜백 함수
 
@@ -14,6 +16,7 @@ class PillBag extends StatefulWidget {
     required this.medicineSeq,
     required this.accountSeq,
     required this.nickname,
+    required this.color,
     required this.isSavedMedicine,
     required this.onClick,
   });
@@ -47,20 +50,69 @@ class _PillBagState extends State<PillBag> {
     //   subtitle: Text(widget.nickname),
     // );
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: ListTile(
-        leading: Checkbox(
-          value: isSavedMedicine,
-          onChanged: (bool? value) {
-            // 체크박스 클릭할때마다 상태 변경
-            setState(() {
-              isSavedMedicine = value!;
-            });
-            widget.onClick(); // 부모 위젯에 이벤트 알림
-          },
+      margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.only(bottom: 5),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Palette.SHADOW_GREY, width: 0.5),
         ),
-        title: Text(widget.envelopName),
-        subtitle: Text(widget.nickname),
+      ),
+      child: ListTile(
+        leading: Transform.scale(
+          scale: 1.5, // 체크박스 크기 조절
+          child: Checkbox(
+            value: isSavedMedicine,
+            onChanged: (bool? value) {
+              // 체크박스 클릭할때마다 상태 변경
+              // api 연결해서 수정하기 !
+              setState(() {
+                isSavedMedicine = value!;
+              });
+              widget.onClick(); // 부모 위젯에 이벤트 알림
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            side: const BorderSide(
+              color: Palette.SHADOW_GREY,
+              width: 1,
+            ),
+            activeColor: Palette.MAIN_BLUE,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+        ),
+        // title: Text(widget.envelopName),
+        // subtitle: Text(widget.nickname),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.envelopName,
+              style: const TextStyle(
+                color: Palette.MAIN_BLACK,
+                fontSize: 17,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(int.parse(widget.color))),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                widget.nickname,
+                style: const TextStyle(
+                  fontSize: 13,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w400,
+                  color: Palette.MAIN_BLACK,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
