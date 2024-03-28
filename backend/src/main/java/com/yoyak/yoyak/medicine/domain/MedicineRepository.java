4387ec9a31422e8763fa9e6a7c8f99bdb0821d0e
신성현 <froggy0m0a@gmail.com>
2024-03-29 00:24:39 +0900

    [refactor] 24.03.29 검색 파라매터 추가
    Change-Id: Id84d34075be12370d16ba48a3f1c4b3b27d68f39

--- backend/src/main/java/com/yoyak/yoyak/medicine/domain/MedicineRepository.java
+++ backend/src/main/java/com/yoyak/yoyak/medicine/domain/MedicineRepository.java
@@ -21,7 +21,8 @@ public interface MedicineRepository extends JpaRepository<Medicine, Long> {
             + "  (COALESCE(:formCodeName, null) IS NULL OR ("
             + "     (:formCodeName = '정제류'   AND md.formCodeName IN ('필름코팅정', '나정', '당의정', '장용성필름코팅정', '장용성당의정', '추어블정(저작정)', '다층정', '서방성필름코팅정', '서방정', '장용정', '발포정', '유핵정', '질정', '트로키제')) OR "
             + "     (:formCodeName = '경질캡슐' AND md.formCodeName IN ('경질캡슐제|산제', '경질캡슐제|과립제', '경질캡슐제|정제', '젤라틴코팅성경질캡슐제')) OR "
-            + "     (:formCodeName = '연질캡슐' AND md.formCodeName IN ('연질캡슐제|현탁상', '연질캡슐제|액상', '장용성필름코팅캡슐제'))"
+            + "     (:formCodeName = '연질캡슐' AND md.formCodeName IN ('연질캡슐제|현탁상', '연질캡슐제|액상', '장용성필름코팅캡슐제')) OR "
+            + "     (:formCodeName = '기타' AND md.formCodeName IN ('장용성캡슐제|펠렛', '장용성캡슐제|정제', '껌제', '서방성캡슐제|펠렛', '지지체가있는첩부제', '구강붕해필름'))"
             + "  )) AND "
             + "  (COALESCE(:line, null) IS NULL OR md.lineFront = :line OR md.lineBack = :line)")
     List<MedicineDto> findByParameters(
