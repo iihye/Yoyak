8963e563e706833118709fd3880d9562b30ddfa5
김희수 <wlslaka4@gmail.com>
2024-04-01 00:40:23 +0900

    sdd
    Change-Id: I4b009a8e15549e387c1ce1c6486548a6ceb9227f

--- ai/extract_information.py
+++ ai/extract_information.py
@@ -1,28 +1,41 @@
 from pymongo import MongoClient
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
-from langchain_community.document_loaders import TextLoader, JSONLoader, DirectoryLoader
-from langchain_community.llms import OpenAI
+from langchain_community.document_loaders import TextLoader, JSONLoader, DirectoryLoader,MongodbLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.chains import RetrievalQA
 import gradio as gr
 from gradio.themes.base import Base
 import key_param
 from pprint import pprint
+import os 
 
-client = MongoClient(key_param.MONGO_URI)
-dbName = "yoyak"
-collectionName = "medicine_detail_vector"
-collection = client[dbName][collectionName]
+# client = MongoClient(key_param.MONGO_URI)
+# dbName = "yoyak"
+# collectionName = "medicine_detail_vector"
+# collection = client[dbName][collectionName]
+# os.environ["GOOGLE_API_KEY"] = key_param.GOOGLE_API_KEY
+# loader = JSONLoader(
+#     file_path="./medicine/medicine_details.json",
+#     jq_schema=".",
+#     text_content=False,
+# )
 
-loader = JSONLoader(
-    file_path="./medicine/medicine_details.json",
-    jq_schema=".",
-    text_content=False,
+# data = loader.load()
+# print(len(data))
+# # embeddings = OpenAIEmbeddings(openai_api_key=key_param.openai_api_key,model="text-embedding-3-small")
+# # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+# # vectorStore = MongoDBAtlasVectorSearch.from_documents(data, embeddings, collection=collection)
+
+loader = MongodbLoader(
+    connection_string=key_param.MONGO_URI,
+    db_name="yoyak",
+    collection_name="medicine_detail"
 )
 
-data = loader.load()
+docs = loader.load()
 
-embeddings = OpenAIEmbeddings(openai_api_key=key_param.openai_api_key,model="text-embedding-3-small")
-
-vectorStore = MongoDBAtlasVectorSearch.from_documents(data, embeddings, collection=collection)
-
+print(len(docs))
+print(docs[0])
--- ai/key_param.py
+++ ai/key_param.py
@@ -1,2 +1,4 @@
 openai_api_key="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"
 MONGO_URI = "mongodb+srv://polya:tollea1324@cluster0.qhkasr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+GOOGLE_API_KEY = "AIzaSyAzhh7zV2GBG3v4r5aETz6WG3J-C5fsFRE%"
+MONGO_URI="mongodb+srv://polya:tollea1324@cluster0.qhkasr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
