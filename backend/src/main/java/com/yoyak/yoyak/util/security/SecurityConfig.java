c252ee405f6fb87021feaf5d650bb25ad5e4f6f4
김희수 <wlslaka4@gmail.com>
2024-04-03 13:40:55 +0900

    [feat] 24.04.03 화이트리스트 수정
    Change-Id: I42df6eaf33989e1dc6ee1ec84d0d4a41f21b04a8

--- backend/src/main/java/com/yoyak/yoyak/util/security/SecurityConfig.java
+++ backend/src/main/java/com/yoyak/yoyak/util/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
         "/api/user/**", "/api/recognition/**", "/api/medicine/**", "/api/medicineDetail/**",
-        "/api/test/**", "/actuator/**", "/api/challenge/all"
+        "/api/test/**", "/actuator/**", "/api/challenge/article/all"
     };
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService customUserDetailsService;
